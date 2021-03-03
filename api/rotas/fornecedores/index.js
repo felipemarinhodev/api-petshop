c979285542706f996e364de6ca5c3835fb94c0e8
Felipe Marinho <felipmarinho@gmail.com>
2021-03-02 21:24:39 -0300

    update status of response

--- api/rotas/fornecedores/index.js
+++ api/rotas/fornecedores/index.js
@@ -4,7 +4,7 @@ const Fornecedor = require('./Fornecedor')
 
 roteador.get('/', async (req, res) => {
 	const resultados = await TabelaFornecedor.listar()
-	res.send(JSON.stringify(resultados))
+	res.status(200).send(JSON.stringify(resultados))
 })
 
 roteador.post('/', async (req, res) => {
@@ -12,7 +12,7 @@ roteador.post('/', async (req, res) => {
 		const dadosRecebidos = req.body
 		const fornecedor = new Fornecedor(dadosRecebidos)
 		await fornecedor.criar()
-		res.send(JSON.stringify(fornecedor))
+		res.status(201).send(JSON.stringify(fornecedor))
 	} catch (erro) {
 		res.status(404).send(JSON.stringify({ mensagem: erro.message}))
 	}
@@ -36,7 +36,7 @@ roteador.put('/:id', async (req, res) => {
 		const dados = Object.assign({}, dadosRecebidos, { id })
 		const fornecedor = new Fornecedor(dados)
 		await fornecedor.atualizar()
-		res.send(JSON.stringify(fornecedor))
+		res.status(200).send(JSON.stringify(fornecedor))
 	} catch (erro) {
 		res.status(400).send(JSON.stringify({ mensagem: erro.message}))
 	}
