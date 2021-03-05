a79df066b89f7a29ae9bbbf55e4f40d08c73b1e8
Felipe Marinho <felipmarinho@gmail.com>
2021-03-05 07:16:45 -0300

    improve SerializadorFornecedor

--- api/Serializador.js
+++ api/Serializador.js
@@ -9,7 +9,6 @@ class Serializador {
 		if (this.contentType === 'application/json') {
 			return this.json(dados)
 		}
-
 		throw new ValorNaoSuportado(this.contentType)
 	}
 }
@@ -18,6 +17,34 @@ class SerializadorFornecedor extends Serializador {
 	constructor(contentType) {
 		super()
 		this.contentType = contentType
+		this.camposPublicos = ['id', 'empresa', 'categoria']
+	}
+
+	serializar (dados) {
+		if (this.contentType === 'application/json') {
+			return this.json(this.filtrar(dados))
+		}
+		throw new ValorNaoSuportado(this.contentType)
+	}
+
+	filtrarObjeto (dados) {
+		const novoObjeto = {}
+
+		this.camposPublicos.forEach(campo => {
+			if (dados.hasOwnProperty(campo)) {
+				novoObjeto[campo] = dados[campo]
+			}
+		})
+		return novoObjeto
+	}
+
+	filtrar (dados) {
+		if (Array.isArray(dados)) {
+			dados = dados.map(item => this.filtrarObjeto(item))
+		} else {
+			dados = this.filtrarObjeto(dados)
+		}
+		return dados
 	}
 }
 
